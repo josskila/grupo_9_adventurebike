4de0f40b526740c8a8bceab62d000be0f003ef7c
Yesic402 <yesic.goez@gmail.com>
2022-12-31 09:52:09 -0500

    levantando servidor y añadiendo HTMLs para 2do sprint

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const publicPath = path.resolve(__dirname,"./public");
+app.use(express.static(publicPath));
+app.listen(3000, ()=>{console.log("Corriendo en puerto 3000")});
+
+app.get("/", (req, res)=>{res.sendFile(path.resolve(__dirname,"./views/home.html"))});
