83cf78c10e2081b29effb2eab8bd1677663b3ad1
Yesic402 <yesic.goez@gmail.com>
2023-01-01 11:34:39 -0500

    editando home

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
-const path = require("path");
-const publicPath = path.resolve(__dirname,"./public");
-app.use(express.static(publicPath));
+app.use(express.static("public"));
 app.listen(3000, ()=>{console.log("Corriendo en puerto 3000")});
 
-app.get("/", (req, res)=>{res.sendFile(path.resolve(__dirname,"./views/home.html"))});
+app.get("/", (req, res)=>{res.sendFile(__dirname + "/views/home.html")});
+app.get("/", (req, res)=>{res.sendFile(__dirname,"/views/product.html")});
+app.get("/", (req, res)=>{res.sendFile(__dirname,"/views/purchase.html")});
+app.get("/", (req, res)=>{res.sendFile(__dirname,"/views/sig-in.html")});
+app.get("/", (req, res)=>{res.sendFile(__dirname,"./views/home.html")});
